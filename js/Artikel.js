eda7a78f5ca393b3ac3db01e3c6a0a4b872e451f
Fradiano Hasani <dianobeats@gmail.com>
2022-05-13 13:31:36 +0200

    mesch .. KLASSE {!

---
+++ js/Artikel.js
@@ -0,0 +1,13 @@
+class Artikel {
+  static counter = 1
+  id = gruppe.counter++
+  index
+  name
+  artikelListe = []
+
+  constructor(name, index) {
+    this.name = name
+    this.index = index
+  }
+
+}
