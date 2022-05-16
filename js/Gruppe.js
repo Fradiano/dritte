6248b61f9979528fa7cee7721a546ede443c53c9
Fradiano Hasani <dianobeats@gmail.com>
2022-05-16 12:07:22 +0200

    läuft nicht :(

---
+++ js/Gruppe.js
@@ -0,0 +1,38 @@
+import {Artikel} from './Artikel'
+import {Shopping} from './Shopping'
+
+
+class Gruppe {
+
+  static counter = 1
+  id = ++
+  name
+  index
+  artikel
+  artikelliste = ["saft", "wasser", "cola"]
+
+  function.artikelAuflisten (gekauft) {
+  for (gekauft of artikelliste)
+    if (artikel.gekauft == true)
+      return artikelListe
+
+
+}
+`${artikel.name}`
+
+
+
+
+constructor(id,name, index, artikel, artikelliste, id) {
+    id = this.counter ++
+
+  }
+}
+
+class Shopping {
+  gruppenListe
+  aktiveGruppe
+  
+}
+
+export {Gruppe}
